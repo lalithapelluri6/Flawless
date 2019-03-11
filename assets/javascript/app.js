cfd3d36001e832738d0027199776baba32184284
KB Total Recall <kanwar@gmail.com>
2019-03-11 13:42:12 -0700

    Add app.js updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,4 +14,38 @@
 // Kanwar
 //
 //
-//
+
+
+function invokeInputForm() {
+
+    // Create the Form 
+    var containerDiv = $("<div>").addClass("container");
+    var inputForm = $("<form>");
+    // Input field for Search location by Zipcode or City
+    var locationFormDiv = $("<div>").addClass("form-group");
+    locationFormDiv.append("<label for='location'> Search by location: </label>" );
+    locationFormDiv.append("<input type='text' id='location' class='form-control'>");
+   
+    // Input field for Restaurants and other places to meet
+    var foodFormDiv = $("<div>").addClass("form-group");
+    foodFormDiv.append("<label for='foodAndDrinks'> What Cuisine : </label>" );
+    foodFormDiv.append("<input type='text' id='foodAndDrinks' placeholder='Eg Asian, American, French, Italian, Indian' class='form-control'>");
+
+    // Input field for field of interest
+    var interestFormDiv = $("<div>").addClass("form-group");
+    interestFormDiv.append("<label for='fieldOfInterest'> Interest : </label>" );
+    interestFormDiv.append("<input type='text' id='fieldOfInterest' placeholder='Eg Industry, Travel, Movie , Sports, Science' class='form-control'>");
+
+    inputForm.append(locationFormDiv,foodFormDiv,interestFormDiv);
+   
+    var submitButton = $("<button>").addClass("btn btn-success m-3 float-right").attr("id", "btn-submit").text("Submit");
+    var clearButton =  $("<button>").addClass("btn btn-warning m-3 float-right").attr("id", "btn-clear").text("Clear");
+    
+    $("#input-form").append(inputForm, submitButton, clearButton);
+    $(containerDiv).append("#input-form");
+
+}
+
+invokeInputForm();
+
+//$(document).on("click",".icon", invokeInputForm);
