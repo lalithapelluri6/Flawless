f775504410601fe10a1e180019d22a88b3344a58
SophieM <47410186+SophM@users.noreply.github.com>
2019-03-11 17:59:25 -0700

    add updated version of app.js - organized the main part of the file with 'function' section, 'main process' section, 'variables' section,...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// -------------------------------------------------------
 // Firebase Configuration & initialization
 //
 var config = {
@@ -12,87 +13,97 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-//   Namita
-//
-//
-//
-//
-//
-//   Sophie
-//
-//
+// ------------------------------------------------------
+// Variables (if needed)
 //
 //
 
-// Kanwar
-//
-//
 
-$(document).ready(function() {
-  
+// ------------------------------------------------------
+// Functions
+//
+// function to display the input form
 function invokeInputForm() {
-  
-    // Create the Form 
-    var containerDiv = $("<div>").addClass("container float-left");
-    var inputForm = $("<form>");
-    // Input field for Search location by Zipcode or City
-    var locationFormDiv = $("<div>").addClass("form-group");
-    locationFormDiv.append("<label for='location'> Search by location: </label>" );
-    locationFormDiv.append("<input type='text' id='location' class='form-control'>");
-   
-    // Input field for Restaurants and other places to meet
-    var foodFormDiv = $("<div>").addClass("form-group");
-    foodFormDiv.append("<label for='foodAndDrinks'> What Cuisine : </label>" );
-    foodFormDiv.append("<input type='text' id='foodAndDrinks' placeholder='Eg Asian, American, French, Italian, Indian' class='form-control'>");
 
-    // Input field for field of interest
-    var moviesFormDiv = $("<div>").addClass("form-group");
-    moviesFormDiv.append("<label for='moviesUpdate'> Entertainment updates : </label>" );
-    moviesFormDiv.append("<input type='text' id='moviesUpdate' placeholder='Eg Name of Movie or TV Shows' class='form-control'>");
+  // Create the Form within a container
+  var containerDiv = $("<div>").addClass("container float-left");
+  var inputForm = $("<form>");
 
-      // Input field for field of interest
-      var newsFormDiv = $("<div>").addClass("form-group");
-      newsFormDiv.append("<label for='newsUpdate'> News updates : </label>" );
-      newsFormDiv.append("<input type='text' id='newsUpdate' placeholder='Eg Industry, Travel, Movie , Sports, Science' class='form-control'>");
-  
+  // Input field for Search location by Zipcode or City
+  var locationFormDiv = $("<div>").addClass("form-group");
+  locationFormDiv.append("<label for='location'> Search by location: </label>");
+  locationFormDiv.append("<input type='text' id='location' class='form-control'>");
 
-    inputForm.append(locationFormDiv,foodFormDiv,moviesFormDiv,newsFormDiv);
-   
-    var submitButton = $("<button>").addClass("btn btn-success m-3 float-right").attr("id", "btn-submit").attr("type","submit").text("Submit");
-    var clearButton =  $("<button>").addClass("btn btn-warning m-3 float-right").attr("id", "btn-clear").text("Clear");
-    
-    $("#input-form").append(inputForm, submitButton, clearButton);
-    $(containerDiv).append("#input-form");
+  // Input field for Restaurants and other places to meet
+  var foodFormDiv = $("<div>").addClass("form-group");
+  foodFormDiv.append("<label for='foodAndDrinks'> What Cuisine : </label>");
+  foodFormDiv.append("<input type='text' id='foodAndDrinks' placeholder='Eg Asian, American, French, Italian, Indian' class='form-control'>");
 
-    $(document).on("click", "button[type=submit]", function (event) {
-      event.preventDefault();
-      alert("pressed Submit");
-      saveToFirebase();
-    });
+  // Input field for news if want latest news 
+  var moviesFormDiv = $("<div>").addClass("form-group");
+  moviesFormDiv.append("<label for='moviesUpdate'> Entertainment updates : </label>");
+  moviesFormDiv.append("<input type='text' id='moviesUpdate' placeholder='Eg Name of Movie or TV Shows' class='form-control'>");
+
+  // Input field for movies or TV Shows if wants info 
+  var newsFormDiv = $("<div>").addClass("form-group");
+  newsFormDiv.append("<label for='newsUpdate'> News updates : </label>");
+  newsFormDiv.append("<input type='text' id='newsUpdate' placeholder='Eg Industry, Travel, Movie , Sports, Science' class='form-control'>");
+
+  // append those input fields to the input form
+  inputForm.append(locationFormDiv, foodFormDiv, moviesFormDiv, newsFormDiv);
+
+  // create the buttons to submit or to clear the textbox fields
+  var submitButton = $("<button>").addClass("btn btn-success m-3 float-right").attr("id", "btn-submit").attr("type", "submit").text("Submit");
+  var clearButton = $("<button>").addClass("btn btn-warning m-3 float-right").attr("id", "btn-clear").text("Clear");
+
+  // append the input form and the button to the container div created above
+  containerDiv.append(inputForm, submitButton, clearButton);
+  // append the input fields and button to the HTML section with ID = input-form
+  $("#input-form").append(containerDiv); 
 }
- 
-  function saveToFirebase() {
-    // store the value entered in form into variables
-     var location = $("#location").val().trim();
-     var foodAndDrinks = $("#foodAndDrinks").val().trim();
-     var moviesUpdate = $("#moviesUpdate").val().trim();
-     var newsUpdate = $("#newsUpdate").val().trim();
 
-      var newSearchStored={
-           location: location,
-           foodAndDrinks: foodAndDrinks,
-           moviesUpdate: moviesUpdate,
-           newsUpdate: newsUpdate
-      };
+// function to save into the firebase the user input
+function saveToFirebase() {
+  // store the value entered in form into variables
+  var location = $("#location").val().trim();
+  var foodAndDrinks = $("#foodAndDrinks").val().trim();
+  var moviesUpdate = $("#moviesUpdate").val().trim();
+  var newsUpdate = $("#newsUpdate").val().trim();
 
-      database.ref().push(newSearchStored);
+  var newSearchStored = {
+    location: location,
+    foodAndDrinks: foodAndDrinks,
+    moviesUpdate: moviesUpdate,
+    newsUpdate: newsUpdate
+  };
 
-  }
+  database.ref().push(newSearchStored);
 
-$(".fas").on("click", function(event) {
+}
+
+
+
+// ------------------------------------------------------------
+// Main process
+
+// get the HTML ready
+$(document).ready(function () {
+
+  // display the input form when an icon has been cliked
+  $(".fas").on("click", function (event) {
     event.preventDefault();
     invokeInputForm();
-});
+  });
 
+  // when the submit has been clicked...
+  $(document).on("click", "button[type=submit]", function (event) {
+    event.preventDefault();
+
+    // save the user input to firebase 
+    saveToFirebase();
+
+    // do the AJAX calls
+
+  });
 
 })
